de252e6eb5904486808e47a7a403e675cb8bb516
Nakerakantianjali <38999810+Nakerakantianjali@users.noreply.github.com>
2020-12-10 19:27:26 +0530

    Update Assignment11.java

--- Assignment11.java
+++ Assignment11.java
@@ -7,16 +7,27 @@ import java.io.*;
 Class Assignment11{
 	public static void main(String [] args)thorws Exception{
 		File file=new File(args[0]);
- 		FileWriter fw=new FileWriter(file.getPath(),true);
-            	BufferedWriter bw=new BufferedWriter(fw);
-            	try {
-                	fr=new FileOutputStream(file.getPath(),true);
-                	for (Map.Entry<Character, Integer> entry : m.entrySet()) {
-                                        bw.write(entry.getKey() +" "+entry.getValue()+"\n");                }
+        FileInputStream fileStream = new FileInputStream(file);
+        InputStreamReader input = new InputStreamReader(fileStream);
+        BufferedReader reader = new BufferedReader(input);
+        String line;
+        Map<Character,Integer>map=new HashMap<Character, Integer>() ;
+        while((line = reader.readLine()) != null)
+        {
+
+            for(int i=0;i<line.length();i++){
+                System.out.println(line);
+
+                if(!map.containsKey(line.charAt(i))){
+                    map.put(line.charAt(i),1);
+                }
+                else{
+
+                    map.put(line.charAt(i),map.get(line.charAt(i))+1);
+                }
             }
-            catch (Exception e){}
-            finally{
-                bw.close();
-            }
-	}
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() +" "+entry.getValue()+"\n");                }
+    }
 }
